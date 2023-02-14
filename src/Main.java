74440c5ab540dcd178408b86819f3b6191e0158d
Henry Malilay <Henry.J.Malilay-1@ou.edu>
2023-02-14 17:03:13 -0600

    feat: allows user to input integers. displays sum/product of array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+	
+    public static void main(String[] args) {
+        
+        int[] userArray;
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("ENTER THE NUMBER OF ELEMENTS: ");
+        userArray = new int[scanner.nextInt()];
+        
+        System.out.println("ENTER THE ELEMENTS: ");
+        for (int i = 0; i < userArray.length; i++) {
+            userArray[i] = scanner.nextInt();
+        }
+        
+        System.out.println("SUM OF THE ELEMENTS: " + addArraySum(userArray));
+        System.out.println("PRODUCTS OF THE ELEMENTS: " + multiplyArrayElements(userArray));
+        
+        scanner.close();
+
+    }
 
     public static int addArraySum(int[] givenArray) {    
         return Arrays.stream(givenArray).sum();
