2b17fe16429e5a9833d4c81aeb4e1f20c30db3a4
Henry Malilay <Henry.J.Malilay-1@ou.edu>
2023-02-14 17:02:20 -0600

    feat: added method to multiply all array elements

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static int addArraySum(int[] givenArray) {    
         return Arrays.stream(givenArray).sum();
     }
+    
+    public static int multiplyArrayElements(int[] givenArray) {
+        return Arrays.stream(givenArray).boxed().reduce(1, (a, b) -> a * b);
+    }
 	
 }
